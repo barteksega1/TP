24201938c9ff29496e4892b064d0f035e1f4cec5
barteksega1 <barto24.sg@gmail.com>
2024-10-12 20:46:59 +0200

    lista-zerowa

---
+++ l0/demo/src/main/java/newproject/CLI.java
@@ -0,0 +1,93 @@
+package newproject;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CLI 
+{
+    public static void run()
+    {
+        Scanner scanner = new Scanner(System.in);
+        CommandList.write();
+        try
+        {
+            while(true)
+            {
+                try
+                {
+                    System.out.print("> ");
+                    String input = scanner.nextLine().trim();
+
+                    if (input.isEmpty()) 
+                    {
+                        continue;
+                    }
+
+                    String parts[] = input.split("\\s+", 3);
+
+                    String command = parts[0];
+                    //Double no1 = Double.parseDouble(parts[1]); pmd czegos uczy...
+                    //Double no2 = Double.parseDouble(parts[2]);
+
+
+                    if(!(command.equals("exit")))
+                    {
+                        if(parts.length < 3)
+                        throw new IllegalArgumentException();
+                    }
+                   
+
+
+                    switch (command)
+                    {
+                        case "add":
+                            System.out.println(Double.parseDouble(parts[1]) + Double.parseDouble(parts[2]));
+                            break;
+
+                        case "substract":
+                            System.out.println(Double.parseDouble(parts[1]) - Double.parseDouble(parts[2]));
+                            break;
+
+                        case "multiply":
+                            System.out.println(Double.parseDouble(parts[1]) * Double.parseDouble(parts[2]));
+                            break;
+                        
+                        case "divide":
+                            System.out.println(Double.parseDouble(parts[1]) / Double.parseDouble(parts[2]));
+                            break;
+                    
+                        case "command_list":
+                            CommandList.write();
+                            break;
+                        
+                        case "exit":
+                            throw new IOException();
+
+                        default:
+                            System.out.println("Invalid command. Type 'command_list' to see the command list. Otherwise fuck off");
+                            break;
+                    }
+
+                                        
+
+                }
+
+                catch(IllegalArgumentException e)
+                {
+                    System.err.println("Invalid argument/Not enough arguments");
+                }
+            }
+
+           
+        }
+        catch (IOException ex)
+        {
+            System.out.println("Client closed \n");
+        }
+
+        finally
+        {
+            scanner.close();
+        }
+    }
+
+}
---
+++ l0/demo/src/main/java/newproject/CommandList.java
@@ -0,0 +1,16 @@
+package newproject;
+
+public final class CommandList 
+{
+    public static void write()
+    {
+        System.out.println("YE OLDE COMMAND LIST \n" + 
+                            "This authority listeth shouldst beest obeyed at all did cost \n" +
+                            "'add' - adds two numbers \n" +
+                            "'substract' - substracts number no.2 from number no.1 \n" +
+                            "'multiply' - multiplies two numbers \n" +
+                            "'divide' - divides number no.1 by number no.2 \n" +
+                            "'exit' - shuts down the application \n");
+    }
+ 
+}
